ed53cdbf80667195a6e0bd8570a42c41b8819d5d
Noham <72859612+Noh4m@users.noreply.github.com>
2023-07-29 01:01:52 +0200

    fix port v2

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const puppeteerScript = require('./index'); // Remplacez par le chemin vers votre script Puppeteer
 
 const PORT = process.env.PORT || 3000;
 
-// Configuration et routes de votre application
+// Configuration et routes de votre application Express
 // Exemple :
 app.get('/', (req, res) => {
   res.send('Hello, World!');
@@ -11,4 +12,5 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
+  puppeteerScript(); // Exécutez votre script Puppeteer ici
 });
