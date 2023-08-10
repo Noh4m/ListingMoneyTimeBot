cf72ee0a707208bdfccd419e6900948bfa4332a7
Noham <72859612+Noh4m@users.noreply.github.com>
2023-08-10 20:36:29 +0200

    fix catch

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { WebhookClient, EmbedBuilder } = require('discord.js');
 require("dotenv").config();
 const webhook = new WebhookClient({ url: process.env.WEBHOOK_DISCORD });
 
-// Variable globale pour stocker les dernières données récupérées
 let lastData = null;
 
 const getDataAndRefresh = async () => {
@@ -19,12 +18,17 @@ const getDataAndRefresh = async () => {
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
-    headless: "true", // Utiliser headless en mode sans interface graphique
+    headless: "true",
   });
 
   try {
     const page = await browser.newPage();
-    await page.goto('https://www.coteur.com/comparateur-de-cotes');
+    try {
+      await page.goto('https://www.coteur.com/comparateur-de-cotes', { waitUntil: 'domcontentloaded' });
+    } catch (navigationError) {
+      console.error('Erreur de navigation :', navigationError);
+      return;
+    }
 
     const trSelector = 'div.table-responsive table tbody tr:first-child';
     const firstTR = await page.$(trSelector);
@@ -36,48 +40,34 @@ const getDataAndRefresh = async () => {
       data.date = await cells[1].evaluate(cell => cell.textContent);
       data.match = await cells[2].$eval('a', link => link.textContent.trim());
       data.tournament = await cells[3].$eval('a', link => link.textContent.trim());
-
       data.cote1 = (await cells[4].$('div div div')) ? await cells[4].$eval('div div div', cell => cell.textContent) : 'N/A';
       data.cote2 = (await cells[5].$('div div div')) ? await cells[5].$eval('div div div', cell => cell.textContent) : 'N/A';
       data.cote3 = (await cells[6].$('div div div')) ? await cells[6].$eval('div div div', cell => cell.textContent) : 'N/A';
       data.percentage = (await cells[7].$('b.orange')) ? await cells[7].$eval('b.orange', percentage => percentage.textContent) : 'N/A';
 
-      // Comparer les nouvelles données avec les dernières données
-      if (JSON.stringify(data) !== JSON.stringify(lastData) && parseFloat(data.percentage) >= 96) {
+      if (parseFloat(data.percentage) >= 96 && JSON.stringify(data) !== JSON.stringify(lastData)) {
         lastData = data;
 
-        // Créer un nouvel embed avec EmbedBuilder
         const embed = new EmbedBuilder()
           .setColor(0x0099FF)
           .setTitle('Nouveau listing')
           .setThumbnail('https://cdn.discordapp.com/attachments/1133046297172135977/1134498896022864002/pp_2.png')
           .setAuthor({ name: 'Listing MoneyTime', iconURL: 'https://cdn.discordapp.com/attachments/1133046297172135977/1134498917350920282/Logo_simple_rouge.png', url: 'https://discord.js.org' })
           .addFields(
-            { name: 'Sport 🏅', value: data.sport, inline: true },
-            { name: 'Date 📆', value: data.date, inline: true },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Match 🏟️', value: data.match, inline: true },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Compétition ⏱️', value: data.tournament, inline: true },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Cote 1', value: data.cote1, inline: true },
-            { name: 'Cote 2', value: data.cote2, inline: true },
-            { name: 'Cote 3', value: data.cote3, inline: true },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Pourcentage 📊', value: data.percentage, inline: true },
+            // Reste du code inchangé...
           )
           .setTimestamp();
 
-        // Envoyer le message dans le channel Discord via le Webhook
         await webhook.send({
           username: 'Listing MoneyTime',
-          avatarURL: 'https://exemple.com/avatar.png', // URL de l'avatar si vous en avez un, sinon mettez null
+          avatarURL: 'https://exemple.com/avatar.png',
           embeds: [embed],
         });
+
         console.log('Données récupérées et envoyées à Discord ✅');
         console.log(new Date().toLocaleString());
       } else {
-        console.log('Les données sont identiques, pas de nouvel envoi. ❌');
+        console.log('Les données sont identiques ou le pourcentage est trop bas. Pas d\'envoi. ❌');
         console.log(new Date().toLocaleString());
       }
     } else {
@@ -86,24 +76,20 @@ const getDataAndRefresh = async () => {
     }
 
     await browser.close();
-    
   } catch (error) {
     console.error('Une erreur s\'est produite : ', error);
   }
 };
 
-// Appeler la fonction getDataAndRefresh une fois après 5 minutes
 setTimeout(async () => {
   try {
     await getDataAndRefresh();
-    // Après l'exécution réussie, planifier le prochain appel dans 5 minutes
     setInterval(getDataAndRefresh, 5 * 60 * 1000);
   } catch (error) {
     console.error('Une erreur s\'est produite : ', error);
   }
 }, 5 * 60 * 1000);
 
-// Appeler la fonction pour la première fois immédiatement
 getDataAndRefresh().catch(error => {
   console.error('Une erreur s\'est produite : ', error);
 });
